7b9d7d1739f48c19d141ff79a79a0658682ac123
DesBasito <116246777+DesBasito@users.noreply.github.com>
2026-03-18 01:13:28 +0600

    Add Cucumber BDD tests, JaCoCo 80% coverage check, and HTML report config

---
+++ src/test/java/abu/epam/com/workloadservice/cucumber/WorkloadComponentTestRunner.java
@@ -0,0 +1,17 @@
+package abu.epam.com.workloadservice.cucumber;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/component")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "abu.epam.com.workloadservice.cucumber.config,abu.epam.com.workloadservice.cucumber.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,html:target/cucumber-workload-report.html")
+public class WorkloadComponentTestRunner {
+}
